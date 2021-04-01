07f4ddda8a4bcc3609fa4e16d778359ca719db09
BigMurloc <s21159@pjwstk.edu.pl>
2021-04-01 13:58:47 +0200

    add command logic

--- src/main/java/commandservice/Command.java
+++ src/main/java/commandservice/Command.java
@@ -2,6 +2,8 @@ package commandservice;
 
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
+import java.io.FileNotFoundException;
+
 @FunctionalInterface
 public interface Command {
     void handle(MessageReceivedEvent event, String[] args);
