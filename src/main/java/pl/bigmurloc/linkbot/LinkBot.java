e9af0f608663d3da1689c0e80b213c84d1680e2f
BigMurloc <s21159@pjwstk.edu.pl>
2021-04-14 12:31:15 +0200

    token as env variable

--- src/main/java/pl/bigmurloc/linkbot/LinkBot.java
+++ src/main/java/pl/bigmurloc/linkbot/LinkBot.java
@@ -1,22 +1,17 @@
 package pl.bigmurloc.linkbot;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import pl.bigmurloc.linkbot.command.CommandHandler;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import javax.security.auth.login.LoginException;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.util.Scanner;
 
 @SpringBootApplication
 public class LinkBot {
-    public static void main(String[] args) throws LoginException, FileNotFoundException {
-        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        InputStream is = classLoader.getResourceAsStream("token");
-        assert is != null;
-        Scanner scanner = new Scanner(is);
-        String token = scanner.nextLine();
+    public static void main(String[] args) throws LoginException {
+        Dotenv dotenv = Dotenv.configure().directory("./.env").load();
+        String token = dotenv.get("TOKEN");
         JDA jda = JDABuilder.createDefault(token).build();
         jda.addEventListener(CommandHandler.getInstance());
         System.out.println(LinkBot.class.getPackageName());
