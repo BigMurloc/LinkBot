6dceb65ef5e905b629d6660e7ef98662c7a4c652
BigMurloc <s21159@pjwstk.edu.pl>
2021-04-18 18:08:16 +0200

    init

--- src/main/java/pl/bigmurloc/linkbot/repository/MessageRepository.java
+++ src/main/java/pl/bigmurloc/linkbot/repository/MessageRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.bigmurloc.linkbot.entity.Message;
 
-import java.util.Optional;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
